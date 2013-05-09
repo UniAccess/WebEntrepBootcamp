73d6779f2a7695f3993b57cd00fe7184b67fdc18
UniAccess <uniaccessapp@gmail.com>
2013-05-08 19:46:55 -0800

    First Upload

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Web Entrepreneurship Workshop | Scotia Chair & UTech Sapna</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+      <!--script for typekit fonts-->
+      <script src="http://use.edgefonts.net/overlock-sc.js"></script>
+      <script src="http://use.edgefonts.net/marvel.js"></script>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="favicon.png">
+  </head>
+
+  <body>
+    <?php include_once("analyticstracking.php") ?>
+    <div class="container">
+
+      <div class="masthead">
+          <h5 class="muted" id="hdr_title_small">The Scotia Chair & The <a href="http://www.utechsapna.com" title="UTech Sapna">UTech Sapna Initiative</a> presents</h5>
+        <h3 id="hdr_title">Web Entrepreneurship Bootcamp</h3>
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="container">
+              <ul class="nav">
+                <li class="active"><a href="#">Registration</a></li>
+                <li><a href="#">About the Workshop</a></li>
+                <li><a href="#">Benefits</a></li>
+                <li><a href="#">Course Content</a></li>
+              </ul>
+            </div>
+          </div>
+        </div><!-- /.navbar -->
+      </div>
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php include 'header.php'?>
+<!-- Jumbotron -->
+<div class="jumbotron row-fluid">
+    <h1>Welcome</h1>
+    <!--<p class="lead">Congratulations on deciding to take the opportunity to learn how to advance your skills, and your business on the web.</p>-->
+    <p class="lead"> Web entrepreneurship is a fascinating journey into an unknown virtual world.
+        You can get lost, hi-jacked, scammed or overwhelmed by your success along this journey.
+             The web entrepreneurship workshop will provide 10 important
+            signposts to guide you on this exciting journey.</p>
+    <form class="form-inline">
+        <div class="">
+            <input class="input-medium" id="appendedInputButton" type="text" placeholder="Email">
+            <button class="btn btn-small btn-success" type="button">Subscribe/Register</button>
+        </div>
+    </form>
+    <!--<a class="btn btn-large btn-success" href="#">Register Now</a>-->
+</div>
+<hr>
+<!-- Example row of columns -->
+<div class="row-fluid">
+    <!--<div class="span2">
+        <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+                <div class="back">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Vestibulum tempor metus at diam bibendum ac molestie tellus tristique. 
+                        Cras sollicitudin ligula id nisl ultricies vel ornare turpis iaculis. 
+                        Nam non nisi ut mauris placerat dictum. 
+                        Cras bibendum, dolor id fringilla consectetur, mauris libero dignissim orci, egestas facilisis metus massa ac ligula. 
+                        Cras sodales purus at lectus commodo mollis. Suspendisse et nunc elit. 
+                        Curabitur accumsan sodales magna, id sodales leo fermentum et. 
+                        Nulla vestibulum, tellus at venenatis tincidunt, metus dolor porta quam, et eleifend velit magna ut nibh. 
+                        Nam rhoncus gravida ipsum eget vulputate. 
+                        Ut nisl eros, tristique nec adipiscing non, iaculis varius turpis. 
+                        Suspendisse ante sem, dignissim non sodales ac, placerat sit amet lacus. 
+                        Sed non enim arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+                        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                        Vivamus tempus, magna in suscipit tempus, felis nunc ornare neque, id pharetra erat massa non velit. 
+                        Vestibulum et purus sed mauris tincidunt tempus eget ac tortor.
+                    </p>
+                </div>
+                <div class="front">
+                    <p>BACK</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="span3">
+        <div class="flip-container left" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+                <div class="front">
+                        <img src="img/question.jpg"/>
+                </div>
+                <div class="back">
+                    THIS GIVES A BRIEF EXPLANATION
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="span3">
+        <div class="flip-container left" ontouchstart="this.classList.toggle('hover');">
+            <div class="flipper">
+                <div class="back">
+                    <p>FRONT
+                        <img src="img/sapnalogo_ftr.png" />
+                    </p>
+                </div>
+                <div class="front">
+                    <p>BACK</p>
+                </div>
+            </div>
+        </div>
+    </div>-->
+            <div class="span4 bg-color">
+                <h2>What is the W.E.B.?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor metus at diam bibendum ac molestie tellus tristique. Cras sollicitudin ligula id nisl ultricies vel ornare turpis iaculis. Nam non nisi ut mauris placerat dictum. Cras bibendum, dolor id fringilla consectetur, mauris libero dignissim orci, egestas facilisis metus massa ac ligula. Cras sodales purus at lectus commodo mollis. Suspendisse et nunc elit. Curabitur accumsan sodales magna, id sodales leo fermentum et. Nulla vestibulum, tellus at venenatis tincidunt, metus dolor porta quam, et eleifend velit magna ut nibh. Nam rhoncus gravida ipsum eget vulputate. Ut nisl eros, tristique nec adipiscing non, iaculis varius turpis. Suspendisse ante sem, dignissim non sodales ac, placerat sit amet lacus. Sed non enim arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus tempus, magna in suscipit tempus, felis nunc ornare neque, id pharetra erat massa non velit. Vestibulum et purus sed mauris tincidunt tempus eget ac tortor. </p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+            <div class="span3">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+             <div class="span3">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+             <div class="span3">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+             <div class="span3">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+                <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div>
+</div>
+<hr>
+<?php include 'footer.php'?>
